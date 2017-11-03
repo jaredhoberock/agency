8cab54adeb3951874964a1aceb2b1a42f9862049
Jared Hoberock <jaredhoberock@gmail.com>
2017-11-03 13:01:51 -0500

    Eliminate superfluous comment

--- agency/functional/bulk_invoke/default_bulk_invoke.hpp
+++ agency/functional/bulk_invoke/default_bulk_invoke.hpp
@@ -40,31 +40,6 @@ struct enable_if_bulk_invoke_execution_policy
 
 } // end detail
 
-///
-/// \defgroup control_structures Control Structures
-/// \brief Control structures create execution.
-///
-///
-/// The primary way Agency programs create execution is by invoking a
-/// **control structure**. Control structures are functions invoked via
-/// composition with an **execution policy**. Execution policies
-/// parameterize control structures by describing the properties of the
-/// requested execution.
-///
-/// For example, the following code snipped uses the bulk_invoke() control
-/// structure with the \ref par execution policy to require the parallel execution
-/// of ten invocations of a lambda function:
-///
-/// ~~~~{.cpp}
-/// using namespace agency;
-///
-/// bulk_invoke(par(10), [](parallel_agent& self)
-/// {
-///   // task body here
-///   ...
-/// });
-/// ~~~~
-
 
 /// \brief Creates a bulk synchronous invocation.
 /// \ingroup control_structures
